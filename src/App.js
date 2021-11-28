c3901be1ff63723ece8d9b2f94975bbb6d6d3b12
jaeyong4536 <jaeyong4536@gmail.com>
2021-11-28 22:54:16 +0900

    rotating words

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Text, OrbitControls } from "@react-three/drei";
 
+const words = [
+  "Apple",
+  "Turtle",
+  "Gang",
+  "Rabbit",
+  "Purple",
+  "React",
+  "Glasses",
+  "Cup",
+  "Book",
+  "Clock",
+  "Phone",
+  "Ambition",
+  "Color",
+  "Origin",
+  "Robot",
+  "Cool",
+];
+const positions = [
+  [-1.5, 0, 0],
+  [1.5, 0, 0],
+  [0, 0, -1.5],
+  [0, 0, 1.5],
+  [0, 1.5, 0],
+  [0, -1.5, 0],
+  [1, 1, 0],
+  [-1, -1, 0],
+  [-1, 0, -1],
+  [1, -1, 0],
+  [-1, 0, 1],
+  [1, 0, 1],
+  [0, 1, -1],
+  [1, 0, -1],
+  [-1, 1, 0],
+  [0.5, 0.5, 0.5],
+];
+
+function Words({ position, text }) {
+  return (
+    <mesh position={position}>
+      <Text scale={[2, 2, 2]} color={"#EC2D2D"} dashed={true}>
+        {text}
+      </Text>
+    </mesh>
+  );
+}
+
+function Scene() {
+  return (
+    <group>
+      {words.map((skill, idx) => {
+        return <Words position={positions[idx]} text={skill} />;
+      })}
+    </group>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <Scene />
+      <OrbitControls autoRotate={true} enableZoom={false} />
+    </Canvas>
   );
 }
 
